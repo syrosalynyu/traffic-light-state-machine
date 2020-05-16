1af8c142e83102386e31f221236a9a4d871355e8
Rosalyn <syrosalynyu@gmail.com>
2020-05-16 12:40:27 -0700

    wave 1

--- src/state-machines/LightMachine.js
+++ src/state-machines/LightMachine.js
@@ -1,7 +1,26 @@
-import { createMachine, interpret } from 'xstate';
+import { Machine, interpret } from 'xstate';
 
-const lightMachine = createMachine({
+const lightMachine = Machine({
   // Your state machine blueprint here
+  id: 'light',
+  initial: 'red',
+  states: {
+    red: {
+      on: {
+        changeLight: 'green',
+      }
+    },
+    green: {
+      on: {
+        changeLight: 'yellow',
+      }
+    },
+    yellow: {
+      on: {
+        changeLight: 'red',
+      }
+    },
+  },
 
 });
 
